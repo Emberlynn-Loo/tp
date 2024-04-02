96cb0603c5893bbc871166579102f0e577dbd9f9
dom <songjunjiedominic@gmail.com>
2024-04-02 13:22:05 +0800

    changed the delete to require a '/' at the start

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -15,6 +15,9 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        return new DeleteCommand(args.trim());
+        if (!args.trim().startsWith("/")){
+            throw new ParseException("Input a UUID prefixed with '/'.");
+        }
+        return new DeleteCommand(args.trim().substring(1));
     }
 }
